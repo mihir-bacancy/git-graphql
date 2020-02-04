5abb1ac34f21063b4b7f337416832a29f3007cde
Mihir bacancy <mihir.kanzariya@bacancytechnology.com>
2020-02-04 20:48:10 +0530

    config upcdated.

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const client_id = "0f1645d52ad054b9bbf0";
-export const redirect_uri = "http://localhost:3000/login/";
-export const client_secret = "41c14d0f7dca502afaac8fc9d18b769f13aa8f99";
+export const client_id = "ADD CLIENT ID";
+export const redirect_uri = "<ADD YOUR REDIRECTED URI>";
+export const client_secret = "ADD CLIENT SECRET";
